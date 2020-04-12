8e8bf09abaf49d0668c636b381e2bcffd45bc19b
Mauro Riva <mauro.riva@finealyze.com>
2020-04-13 01:11:58 +0200

    update the environmental variable names

--- resources/run-pipeline.sh
+++ resources/run-pipeline.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python pipeline.py --input_subs_esp32=$PROJ_SUBS_ESP32 --input_subs_owm=$PROJ_SUBS_OWM --project=$PROJ_NAME --kind=$PROJ_DBKIND --streaming
+python pipeline.py --input_subs=$PROJ_SUBS --project=$PROJ_NAME --kind=$PROJ_DBKIND --streaming
